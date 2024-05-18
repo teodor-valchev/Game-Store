b8ce2e15a10afa6f7930b500160434c452d9ad00
teodor-valchev <teo.thenx@gmail.com>
2024-05-18 11:41:23 +0300

    added 404 page for wrong requests

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    res.render('home')
-})
+router.get("/", (req, res) => {
+    res.render("home");
+});
 
-module.exports = router
+router.get("/404", (req, res) => {
+    res.render("404");
+});
+
+module.exports = router;
--- src/router.js
+++ src/router.js
@@ -3,4 +3,8 @@ const homeController = require("./controllers/homeController");
 
 router.use('/', homeController)
 
+router.use('*', (req, res) => {
+    res.redirect('/404')
+})
+
 module.exports = router
