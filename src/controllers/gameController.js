1f5c1048a1c534df0bbde51f67ddd20cb651bc89
teodor-valchev <teo.thenx@gmail.com>
2024-05-18 16:17:46 +0300

    edit and delete functionality implemented

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,4 +1,10 @@
-const { createGame, getAll, getGame } = require("../services/gameService");
+const {
+    createGame,
+    getAll,
+    getGame,
+    editGame,
+    deleteGame,
+} = require("../services/gameService");
 
 const router = require("express").Router();
 
@@ -29,8 +35,42 @@ router.post("/create", async (req, res) => {
 router.get("/details/:gameId", async (req, res) => {
     const gameId = req.params.gameId;
     const game = await getGame(gameId).lean();
+    const isOwner = req.user?._id === game.owner.toString();
 
-    res.render("game/details", { game });
+    res.render("game/details", { game, isOwner });
+});
+
+router.get("/edit/:gameId", async (req, res) => {
+    const gameId = req.params.gameId;
+    const game = await getGame(gameId).lean();
+
+    res.render("game/edit", { game });
+});
+
+router.post("/edit/:gameId", async (req, res) => {
+    const { platform, name, image, price, genre, description } = req.body;
+    const gameData = {
+        platform,
+        name,
+        image,
+        price,
+        genre,
+        description,
+        owner: req.user._id,
+    };
+    const gameId = req.params.gameId;
+
+    await editGame(gameId, gameData);
+
+    res.redirect(`/games/details/${gameId}`);
+});
+
+router.get("/delete/:gameId", async (req, res) => {
+    const gameId = req.params.gameId;
+
+    await deleteGame(gameId);
+
+    res.redirect("/games/catalog");
 });
 
 module.exports = router;
--- src/services/gameService.js
+++ src/services/gameService.js
@@ -4,4 +4,8 @@ exports.createGame = (gameData) => Game.create(gameData);
 
 exports.getAll = () => Game.find({});
 
-exports.getGame = (gameId) => Game.findById(gameId);
+exports.getGame = (id) => Game.findById(id);
+
+exports.editGame = (id, gameData) => Game.findByIdAndUpdate(id, gameData);
+
+exports.deleteGame = (id) => Game.findByIdAndDelete(id)
